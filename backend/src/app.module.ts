1be291f19d1cc710e17c019906689b480be57f6b
silius124 <silius125@yahoo.com>
2026-02-27 17:18:55 +0300

    feat: add @nestjs/config for .env support

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    AuthModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from './generated/client';
+import { ConfigService } from '@nestjs/config';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 
@@ -8,8 +9,10 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  constructor() {
-    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  constructor(private configService: ConfigService) {
+    const pool = new Pool({
+      connectionString: configService.get<string>('DATABASE_URL'),
+    });
     const adapter = new PrismaPg(pool);
     super({ adapter });
   }
