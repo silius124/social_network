d9797cf08147508055b2bfceb7cc48fb436ab41d
silius124 <silius125@yahoo.com>
2026-03-07 23:22:09 +0300

    feat: handle unique constraint for email and username

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,6 +1,8 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { RegisterDto } from './DTO/register.dto';
+import { Prisma } from 'src/prisma/generated/client';
+import { ConflictException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
@@ -8,29 +10,30 @@ export class AuthService {
   constructor(private prisma: PrismaService) {}
 
   async register(dto: RegisterDto) {
-    const existingUser = await this.prisma.user.findUnique({
-      where: { email: dto.email },
-    });
-
-    if (existingUser) {
-      throw new BadRequestException('Email already exists');
-    }
-
     const hashedPassword = await bcrypt.hash(dto.password, 10);
 
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        password: hashedPassword,
-        username: dto.username,
-        firstName: dto.firstName,
-        lastName: dto.lastName,
-      },
-    });
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: hashedPassword,
+          username: dto.username,
+          firstName: dto.firstName,
+          lastName: dto.lastName,
+        },
+      });
 
-    return {
-      message: 'User created successfully',
-      userId: user.id,
-    };
+      return user;
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      ) {
+        throw new ConflictException(
+          'User with this email or username already exists.',
+        );
+      }
+      throw error;
+    }
   }
 }
