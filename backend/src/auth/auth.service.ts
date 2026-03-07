b7a189bc8f0c1af2ee7ae5bc5d13e9e0037884af
silius124 <silius125@yahoo.com>
2026-03-07 23:36:59 +0300

    feat: add login logic with jwt token

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -2,12 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { RegisterDto } from './DTO/register.dto';
 import { Prisma } from 'src/prisma/generated/client';
+import { JwtService } from '@nestjs/jwt';
+import { UnauthorizedException } from '@nestjs/common';
 import { ConflictException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
+import { LoginDto } from './DTO/login.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
   async register(dto: RegisterDto) {
     const hashedPassword = await bcrypt.hash(dto.password, 10);
@@ -35,5 +41,32 @@ export class AuthService {
       }
       throw error;
     }
+  }
+
+  async login(dto: LoginDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) throw new UnauthorizedException('Invalid credentials');
+
+    const isPasswordValid = await bcrypt.compare(dto.password, user.password);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const payload = {
+      userId: user.id,
+      email: user.email,
+    };
+
+    const token = await this.jwtService.signAsync(payload);
+
+    return {
+      access_token: token,
+    };
   }
 }
