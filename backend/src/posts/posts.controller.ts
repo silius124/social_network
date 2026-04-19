d398f7f2fc0519759b0413dd267636fe7643c431
silius124 <silius125@yahoo.com>
2026-04-19 23:20:46 +0300

    feat(posts): add create and findAll endpoints to comtroller

--- backend/src/posts/posts.controller.ts
+++ backend/src/posts/posts.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { PostsService } from './posts.service';
+import { JwtGuard } from 'src/auth/guard/jwt.guard';
+import { CurrentUser } from 'src/auth/decorator/current-user.decorator';
+import { CreatePostDto } from './dto/posts.dto';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
+
+  @Post()
+  @UseGuards(JwtGuard)
+  create(@CurrentUser('userId') userId: number, @Body() dto: CreatePostDto) {
+    return this.postsService.create(userId, dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.postsService.findAll();
+  }
 }
