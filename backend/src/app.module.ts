290430a51c10289181f3b4fbe967a87c9c580114
silius124 <silius125@yahoo.com>
2026-04-19 21:22:16 +0300

    feat(posts): generate posts module, service, controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
     AuthModule,
     PrismaModule,
+    PostsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
