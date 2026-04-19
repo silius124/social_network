2e7480c77feede749f8b2db27c11ea870cd8fd2f
silius124 <silius125@yahoo.com>
2026-04-19 22:38:11 +0300

    feat(post): implement create post logic in service

--- backend/src/posts/posts.service.ts
+++ backend/src/posts/posts.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreatePostDto } from './dto/posts.dto';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(userId: number, dto: CreatePostDto) {
+    return this.prisma.post.create({
+      data: {
+        ...dto,
+        userId,
+      },
+    });
+  }
+
+  async findAll() {
+    return await this.prisma.post.findMany({
+      include: {
+        user: {
+          select: {
+            id: true,
+            username: true,
+            avatarUrl: true,
+          },
+        },
+      },
+      orderBy: { createdAt: 'desc' },
+    });
+  }
+}
