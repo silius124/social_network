4c609a0bb0b81534786622bac8c3e3bd82d577fb
silius124 <silius125@yahoo.com>
2026-04-25 22:51:30 +0300

    feat(chat): add controller for fetching chat list and messages

---
+++ backend/src/chat/chat.controller.ts
@@ -0,0 +1,26 @@
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtGuard } from 'src/auth/guard/jwt.guard';
+import { ChatService } from './chat.service';
+import { CurrentUser } from 'src/auth/decorator/current-user.decorator';
+
+@Controller('chats')
+@UseGuards(JwtGuard)
+export class ChatController {
+  constructor(private readonly chatService: ChatService) {}
+
+  @Get()
+  getChats(@CurrentUser('userId') userId: number) {
+    return this.chatService.getMyChats(userId);
+  }
+
+  @Get(':id/messages')
+  getMessages(@Param('id', ParseIntPipe) chatId: number) {
+    return this.chatService.getMessages(chatId);
+  }
+}
--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -3,9 +3,11 @@ import { ChatService } from './chat.service';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { ChatGateway } from './chat.gateway';
+import { ChatController } from './chat.controller';
 
 @Module({
   providers: [ChatService, ChatGateway],
   imports: [JwtModule, PrismaModule],
+  controllers: [ChatController],
 })
 export class ChatModule {}
