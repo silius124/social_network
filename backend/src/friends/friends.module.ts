2e6cbed381928c6dcfca842dfbb3a71a0d5fa93a
silius124 <silius125@yahoo.com>
2026-04-27 22:55:42 +0300

    fix notification service

--- backend/src/friends/friends.module.ts
+++ backend/src/friends/friends.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FriendsService } from './friends.service';
 import { FriendsController } from './friends.controller';
+import { NotificationsModule } from 'src/notifications/notifications.module';
 
 @Module({
+  imports: [NotificationsModule],
   controllers: [FriendsController],
   providers: [FriendsService],
 })
--- backend/src/posts/posts.module.ts
+++ backend/src/posts/posts.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
+import { NotificationsModule } from 'src/notifications/notifications.module';
 
 @Module({
+  imports: [NotificationsModule],
   controllers: [PostsController],
   providers: [PostsService],
 })
